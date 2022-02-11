d2def0d49a68676bb70016ee1ffe8928b2ff87d8
Leonid Haimov <8o8inSquares@gmail.com>
2022-02-11 22:30:02 +0200

    Change style of a sidebar

--- src/components/ScriptsSidebar/ScriptsSidebar.js
+++ src/components/ScriptsSidebar/ScriptsSidebar.js
@@ -1,57 +1,95 @@
-import React, {useState} from 'react';
-import s, { container, scriptButtonContainer, divButton } from './ScriptsSidebar.module.css';
-import {IoMdPower} from 'react-icons/io';
+import React, { useState } from "react";
+import s, {
+	container,
+	scriptButtonContainer,
+	divButton,
+} from "./ScriptsSidebar.module.css";
+import { IoMdPower } from "react-icons/io";
 
-const ScriptsSidebar = ({ scripts, onSelect, onToggle, loading, selectedScript }) => {
-    const [searchText, setSearchText] = useState("");
-    if (loading) return <div className={container}>Loading...</div>;
-    return (
-        <div className={container}>
-            <input 
-                type="text" 
-                placeholder="Search" 
-                value={searchText}
-                onChange={(e) => setSearchText(e.target.value)}
-            />
-            <CreateScriptButton
-                script={{
-                    name: "Create New",
-                    new: true,
-                    enabled: false,
-                    id: 0,
-                }}
-                onSelect={onSelect}
-            />
-            {
-                scripts.filter(current => current.name.includes(searchText)).map(script =>
-                    <ScriptButton
-                        key={script.id}
-                        script={script}
-                        onSelect={onSelect}
-                        onToggle={onToggle}
-                        selected={selectedScript && selectedScript.id === script.id}
-                    />
-                )
-            }
-        </div>
-    );
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import Switch from "@mui/material/Switch";
+
+const ScriptsSidebar = ({
+	scripts,
+	onSelect,
+	onToggle,
+	loading,
+	selectedScript,
+}) => {
+	const [searchText, setSearchText] = useState("");
+	if (loading) return <div className={container}>Loading...</div>;
+	return (
+		<div className={container}>
+			<input
+				type="text"
+				placeholder="Search"
+				value={searchText}
+				onChange={(e) => setSearchText(e.target.value)}
+			/>
+			<Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+				<List>
+					<ListItem disablePadding>
+						<ListItemButton
+							dense
+							onClick={() =>
+								onSelect({
+									new: true,
+									enabled: false,
+									id: 0,
+								})
+							}
+						>
+							<ListItemText primary="Create Script" />
+						</ListItemButton>
+					</ListItem>
+					{scripts
+						.filter((current) => current.name.includes(searchText))
+						.map((script) => (
+							<ScriptButton
+								key={script.id}
+								script={script}
+								onSelect={onSelect}
+								onToggle={onToggle}
+								selected={selectedScript && selectedScript.id === script.id}
+							/>
+						))}
+				</List>
+			</Box>
+		</div>
+	);
 };
 
 const ScriptButton = ({ script, onSelect, onToggle, selected }) => {
-    return (
-        <div className={scriptButtonContainer}>
-            <button className={`${selected ? s.selectedScript : ''}`} style={{ width: "100%" }} onClick={() => onSelect(script)}>{script.name}</button>
-            <div className={`${divButton} ${selected ? s.selectedScript : ''}`} onClick={() => onToggle(script)}><IoMdPower color={script.enabled ? "cyan" : "gray"}/></div>
-        </div>
-    );
-};
-
-const CreateScriptButton = ({ script, onSelect }) => {
-    return (
-        <div className={scriptButtonContainer}>
-            <button style={{ width: "100%", background: "#105d59" }} onClick={() => onSelect(script)}>{script.name}</button>
-        </div>
-    );
+	return (
+		<>
+			<ListItem
+				disablePadding
+				secondaryAction={
+					<Switch
+						size="small"
+						label="status"
+						checked={script.enabled}
+						onChange={() => onToggle(script)}
+						defaultChecked
+					/>
+				}
+			>
+				<ListItemButton
+					selected={selected}
+					dense
+					onClick={() => onSelect(script)}
+				>
+					<ListItemText primary={script.name} />
+				</ListItemButton>
+			</ListItem>
+		</>
+	);
 };
 
 export default ScriptsSidebar;
